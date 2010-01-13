11ff11f7b0704767b01250e9f4b0f84ed563b254
jamescollins <jamescollins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-13 05:33:40 +0000

    remove svn info because it doesn't seem to work
    git-svn-id: https://plugins.svn.wordpress.org/weatherzone/trunk@193488 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- weatherzone.php
+++ weatherzone.php
@@ -1,9 +1,5 @@
 <?php
 /*
-	$Id$
-*/
-
-/*
 Plugin Name: WeatherZone Embed
 Plugin URI: http://om4.com.au/wordpress-plugins/weatherzone/
 Description: Allows you to embed WeatherZone.com.au weather buttons on your site. Supports both weather forecast and current weather observations buttons.
